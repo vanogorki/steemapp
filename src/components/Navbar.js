bd17cc260fa3a88f87408c3a487e80cfdab2bbe7
vanogorki <vanogorki@gmail.com>
2024-08-22 19:28:23 +0400

    add side bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,30 @@ import React from "react";
 import logo from "../assets/logo.png";
 import "./Navbar.css";
 import { LuMenu } from "react-icons/lu";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <div class="container">
       <div class="menu__container">
         <LuMenu />
+        <ul class="sidebar">
+          <li>
+            <a href="#">Notifications</a>
+          </li>
+          <li>
+            <a href="#">Store</a>
+          </li>
+          <li>
+            <a href="#">Community</a>
+          </li>
+          <li>
+            <a href="#">Chat</a>
+          </li>
+          <li>
+            <a href="#">Support</a>
+          </li>
+        </ul>
       </div>
 
       <div class="logo__container">
